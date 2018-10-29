59283043d363fc3e2f0221ae178f7e20de842d52
philbranding <philbranding@gmail.com>
2018-10-29 21:00:35 +0100

    created the Camera_Node

--- framework/source/scene_graph.cpp
+++ framework/source/scene_graph.cpp
@@ -1,5 +1,10 @@
 #include "scene_graph.hpp"
 
+/*Screengraph 
+
+
+*/
+
 scene_graph::scene_graph ():
     name_ {"Default Scene Name"},
     root_ {}
@@ -13,7 +18,7 @@ std::string scene_graph::get_name() const
     return name_; 
 }
 
-Node scene_graph::get_root() const
+node scene_graph::get_root() const
 {
     return root_;
 }
@@ -23,7 +28,7 @@ void scene_graph::setName(std::string name)
     this->name_ = name;
 }
 
-void scene_graph::setRoot(Node root)
+void scene_graph::setRoot(node root)
 {
     this->root_ = root;  
 }
